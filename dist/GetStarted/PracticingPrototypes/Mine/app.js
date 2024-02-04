847270361da2a497b8601fad7b092e3d20a7991a
Masoud <masoud.zeinalabedini@gmail.com>
2024-02-04 14:48:25 +0330

    test PracticingPrototypes

--- dist/GetStarted/PracticingPrototypes/Mine/app.js
+++ dist/GetStarted/PracticingPrototypes/Mine/app.js
@@ -1,39 +1,42 @@
 "use strict";
-function randMax(max) {
-    return Math.trunc(1e9 * Math.random()) % max;
-}
-let reel = {
-    symbols: ["X", "Y", "Z", "W", "$", "*", "<", "@"],
-    position: randMax(7),
-    spin() {
-        this.position = (this.position + 100 + randMax(100)) % this.symbols.length;
-    },
-    display() {
-        if (this.position == null) {
-            this.position = randMax(this.symbols.length - 1);
-        }
-        return this.symbols[this.position];
-    },
-};
-let slotMachine = {
-    reels: [Object.create(reel), Object.create(reel), Object.create(reel)],
-    spin() {
-        this.reels.forEach((reel) => {
-            reel.spin();
-        });
-    },
-    display() {
-        this.reels.forEach((reel) => {
-            let printArr = [];
-            reel.spin();
-            const fixedPosition = reel.position;
-            for (let i = -1; i < 2; i++) {
-                reel.position =
-                    (reel.symbols.length + fixedPosition + i) % reel.symbols.length;
-                printArr.push(reel.display());
+(() => {
+    function randMax(max) {
+        return Math.trunc(1e9 * Math.random()) % max;
+    }
+    let reel = {
+        symbols: ["X", "Y", "Z", "W", "$", "*", "<", "@"],
+        position: randMax(7),
+        spin() {
+            this.position =
+                (this.position + 100 + randMax(100)) % this.symbols.length;
+        },
+        display() {
+            if (this.position == null) {
+                this.position = randMax(this.symbols.length - 1);
             }
-            console.log(printArr.join(" | "));
-        });
-    },
-};
-slotMachine.display();
+            return this.symbols[this.position];
+        },
+    };
+    let slotMachine = {
+        reels: [Object.create(reel), Object.create(reel), Object.create(reel)],
+        spin() {
+            this.reels.forEach((reel) => {
+                reel.spin();
+            });
+        },
+        display() {
+            this.reels.forEach((reel) => {
+                let printArr = [];
+                reel.spin();
+                const fixedPosition = reel.position;
+                for (let i = -1; i < 2; i++) {
+                    reel.position =
+                        (reel.symbols.length + fixedPosition + i) % reel.symbols.length;
+                    printArr.push(reel.display());
+                }
+                console.log(printArr.join(" | "));
+            });
+        },
+    };
+    slotMachine.display();
+})();
--- src/GetStarted/PracticingPrototypes/Mine/app.ts
+++ src/GetStarted/PracticingPrototypes/Mine/app.ts
@@ -1,54 +1,57 @@
-type Reel = {
-  symbols: string[];
-  position: number;
-  spin(): void;
-  display(): string;
-};
-interface slotMachine {
-  reels: Reel[];
-  spin: () => void;
-  display: () => void;
-}
+(() => {
+  type Reel = {
+    symbols: string[];
+    position: number;
+    spin(): void;
+    display(): string;
+  };
+  interface SlotMachine {
+    reels: Reel[];
+    spin: () => void;
+    display: () => void;
+  }
 
-function randMax(max: number) {
-  return Math.trunc(1e9 * Math.random()) % max;
-}
+  function randMax(max: number) {
+    return Math.trunc(1e9 * Math.random()) % max;
+  }
 
-let reel: Reel = {
-  symbols: ["X", "Y", "Z", "W", "$", "*", "<", "@"],
-  position: randMax(7),
-  spin() {
-    this.position = (this.position + 100 + randMax(100)) % this.symbols.length;
-  },
-  display() {
-    if (this.position == null) {
-      this.position = randMax(this.symbols.length - 1);
-    }
-    return this.symbols[this.position];
-  },
-};
-
-let slotMachine: slotMachine = {
-  reels: [Object.create(reel), Object.create(reel), Object.create(reel)],
-  spin() {
-    this.reels.forEach((reel) => {
-      reel.spin();
-    });
-  },
-  display() {
-    this.reels.forEach((reel) => {
-      let printArr = [];
-      reel.spin();
-      const fixedPosition = reel.position;
-      for (let i = -1; i < 2; i++) {
-        reel.position =
-          (reel.symbols.length + fixedPosition + i) % reel.symbols.length;
-
-        printArr.push(reel.display());
+  let reel: Reel = {
+    symbols: ["X", "Y", "Z", "W", "$", "*", "<", "@"],
+    position: randMax(7),
+    spin() {
+      this.position =
+        (this.position + 100 + randMax(100)) % this.symbols.length;
+    },
+    display() {
+      if (this.position == null) {
+        this.position = randMax(this.symbols.length - 1);
       }
-      console.log(printArr.join(" | "));
-    });
-  },
-};
+      return this.symbols[this.position];
+    },
+  };
 
-slotMachine.display();
+  let slotMachine: SlotMachine = {
+    reels: [Object.create(reel), Object.create(reel), Object.create(reel)],
+    spin() {
+      this.reels.forEach((reel) => {
+        reel.spin();
+      });
+    },
+    display() {
+      this.reels.forEach((reel) => {
+        let printArr = [];
+        reel.spin();
+        const fixedPosition = reel.position;
+        for (let i = -1; i < 2; i++) {
+          reel.position =
+            (reel.symbols.length + fixedPosition + i) % reel.symbols.length;
+
+          printArr.push(reel.display());
+        }
+        console.log(printArr.join(" | "));
+      });
+    },
+  };
+
+  slotMachine.display();
+})();
