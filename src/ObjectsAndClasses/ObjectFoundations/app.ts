671c1ecb4c1351d0a35aafc2f163fa822123c1dd
Masoud <masoud.zeinalabedini@gmail.com>
2024-04-05 10:51:04 +0330

    Test some example of ObjectsAndClasses\ObjectFoundations

---
+++ src/ObjectsAndClasses/ObjectFoundations/app.ts
@@ -0,0 +1,31 @@
+const testObj = {};
+const obj = {
+  42: 42,
+  true: "True",
+  [testObj]: "[testObj]",
+};
+
+console.log("test");
+
+for (let keyName in obj) {
+  console.log("keyName is:");
+  console.log(keyName);
+  console.log("================");
+
+  console.log("type of keyName is:");
+  console.log(typeof keyName);
+  console.log("================");
+
+  if (keyName === "42") {
+    console.log("Is keyName equal to number 42 ? ");
+    console.log(keyName === 42);
+    console.log("================");
+  }
+
+  if (keyName === "true") {
+    console.log("Is keyName equal to boolean true ? ");
+    console.log(keyName === true);
+    console.log("================");
+  }
+  console.log("----------------************----------------");
+}
