8d2987abea5c9e88bf5affc03820ce533567bde8
Masoud <masoud.zeinalabedini@gmail.com>
2024-05-23 10:00:54 +0330

    Add symbol and Date examples

--- src/TypesAndGrammar/Natives/app.ts
+++ src/TypesAndGrammar/Natives/app.ts
@@ -4,4 +4,18 @@
 
   let arr = Array(3);
   console.log(arr);
+
+  console.log(new Date());
+  console.log(new Date().getTime());
+  console.log(Date.now());
+  console.log(Date());
+
+  const mySym = Symbol("Test Symbol!");
+  console.log(mySym);
+
+  let objectWithSymbol: any = {};
+  objectWithSymbol[mySym] = "Test object with symbol!";
+  objectWithSymbol.a = "Test object with symbol!";
+
+  console.log(objectWithSymbol);
 }
