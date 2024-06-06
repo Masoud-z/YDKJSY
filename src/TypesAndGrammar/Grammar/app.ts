890803b4208b2a66a5e25d303df4424552214570
Masoud <masoud.zeinalabedini@gmail.com>
2024-06-06 09:31:07 +0330

    Add Contextual Rules Labels example

---
+++ src/TypesAndGrammar/Grammar/app.ts
@@ -0,0 +1,22 @@
+foo: for (var i = 0; i < 4; i++) {
+  for (var j = 0; j < 4; j++) {
+    // whenever the loops meet, continue outer loop
+    if (j == i) {
+      // jump to the next iteration of
+      // the `foo` labeled-loop
+      continue foo;
+    }
+    // skip odd multiples
+    if ((j * i) % 2 == 1) {
+      // normal (nonlabeled) `continue` of inner loop
+      continue;
+    }
+    console.log(i, j);
+  }
+}
+
+// 1 0
+// 2 0
+// 2 1
+// 3 0
+// 3 2
