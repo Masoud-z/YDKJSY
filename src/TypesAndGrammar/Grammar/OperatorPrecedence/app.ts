dcdabda5fe73f716e682451489ef3b011c9fc7bb
Masoud <masoud.zeinalabedini@gmail.com>
2024-06-19 09:24:27 +0330

    Add Associativity example

--- src/TypesAndGrammar/Grammar/OperatorPrecedence/app.ts
+++ src/TypesAndGrammar/Grammar/OperatorPrecedence/app.ts
@@ -4,3 +4,12 @@
   let c = false;
   console.log((a && b) || c ? (c || b ? a : c && b) : a);
 }
+
+{
+  let a = 42;
+  let b = "foo";
+  let c = false;
+
+  let d = (a && b) || c ? (c || b ? a : c && b) : a;
+  console.log("d is: ", d);
+}
